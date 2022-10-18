bbe33a6792c2d8eac64f82e260edd0e6cc766123
MaryIsang4 <Maryisang2018@gmail.com>
2022-10-18 02:33:51 -0700

    Printf.c

--- _printf.c
+++ _printf.c
@@ -40,3 +40,52 @@ int _printf(const char *format, ...)
 			printed_chars++;
 		}
 
+		else
+
+		{
+			print_buffer(buffer, &buff_ind);
+
+			flags = get_flags(format, &i);
+
+			width = get_width(format, &i, list);
+
+			precision = get_precision(format, &i, list);
+
+			size = get_size(format, &i);
+
+			++i;
+
+			printed = handle_print(format, &i, list, buffer,
+
+				flags, width, precision, size);
+
+			if (printed == -1)
+
+				return (-1);
+
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+
+void print_buffer(char buffer[], int *buff_ind)
+
+{
+	if (*buff_ind > 0)
+
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
