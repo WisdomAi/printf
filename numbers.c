02d8f37a4473705e20d7b5f9d40a8a5b1f568a63
WisdomAi <sagewhizzy@gmail.com>
2022-10-18 17:24:48 -0700

    collaboration

--- numbers.c
+++ numbers.c
@@ -98,3 +98,33 @@ int print_number_right_shift(char *str, params_t *params)
 	n += _puts(str);
 	return (n);
 }
+
+/**
+ * print_number_left_shift - prints a number with options
+ * @str: the base number as a string
+ * @params: the parameter struct
+ *
+ * Return: chars printed
+ */
+int print_number_left_shift(char *str, params_t *params)
+{
+	unsigned int n = 0, neg, neg2, i = _strlen(str);
+	char pad_char = ' ';
+
+	if (params->zero_flag && !params->minus_flag)
+		pad_char = '0';
+	neg = neg2 = (!params->unsign && *str == '-');
+	if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
+		str++;
+	else
+		neg = 0;
+
+	if (params->plus_flag && !neg2 && !params->unsign)
+		n += _putchar('+'), i++;
+	else if (params->space_flag && !neg2 && !params->unsign)
+		n += _putchar(' '), i++;
+	n += _puts(str);
+	while (i++ < params->width)
+		n += _putchar(pad_char);
+	return (n);
+}
